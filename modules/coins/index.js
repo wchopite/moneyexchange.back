9cc9c05a52dbe26e438e6f7901bb708b7a7b73b0
Wladimir Chopite <wchopite@gmail.com>
2019-06-04 00:12:19 -0300

    [coins module] Manage errors Rejected Promises

--- modules/coins/index.js
+++ modules/coins/index.js
@@ -4,13 +4,13 @@ const coinsManager = require('./coinsManager');
 let _logger;
 
 const handler = {
-  async listCoins(req, res) {
+  async listCoins(req, res, next) {
     try {
       let result = await coinsManager.list({});
       res.json(result);
     } catch(error) {
       _logger.error({caller: 'coins.list', payload: {}, error});
-      res.status(500).json({message: 'Error trying to get the documents'});
+      next(error);
     }
   }
 };
