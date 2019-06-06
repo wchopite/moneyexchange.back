36c58106c2b9e8891c8e0cd06e036469b25cd115
Wladimir Chopite <wchopite@gmail.com>
2019-06-06 00:26:25 -0300

    [validate input data coins conversion endpoints]

--- modules/coinsConversion/index.js
+++ modules/coinsConversion/index.js
@@ -2,15 +2,37 @@
 const conversionManager = require('./conversionManager');
 
 let _logger;
+let _schemaValidator;
+
+const inputSchema = {
+  convertSchema: {
+    type: 'object',
+    required: ['value','base', 'to'],
+    properties: {
+      value: { type: 'number' },
+      base: { type: 'string', minLength: 2, maxLength: 4 },
+      to: { type: 'string', minLength: 2, maxLength: 4 }
+    }
+  },
+  conversionSchema: {
+    type: 'object',
+    required: ['conversionFactor','base', 'to'],
+    properties: {
+      conversionFactor: { type: 'number' },
+      base: { type: 'string', minLength: 2, maxLength: 4 },
+      to: { type: 'string', minLength: 2, maxLength: 4 }
+    }
+  }
+};
 
 const handler = {
   async convert(req, res) {
-    let payload = req.body || null;
-    let { base, to, value } = payload;
+    let { base, to, value } = req.body;
 
-    if (!payload) {
-      _logger.info({caller: 'coinsConvertion.save', payload });
-      res.status(400).json({message: 'You must indicate all necessary data'});
+    const { isValid, validator } = _schemaValidator.validate(Object.assign({},inputSchema.convertSchema), { base, to, value });
+    if(!isValid) {
+      _logger.info('Input data validation',{ payload: req.body, errors: validator.errors });
+      res.status(400).json({message: 'You must indicate the correct data'});
       return;
     }
 
@@ -18,21 +40,24 @@ const handler = {
     res.json({ result });
   },
   async save(req, res) {
-    let payload = req.body || null;
+    let { base, to, conversionFactor } = req.body;
 
-    if (!payload) {
-      _logger.info({caller: 'coinsConvertion.save', payload });
-      res.status(400).json({message: 'You must indicate all necessary data'});
+    const { isValid, validator } = _schemaValidator.validate(Object.assign({},inputSchema.conversionSchema), {base, to, conversionFactor});
+    if(!isValid) {
+      _logger.info('Input data validation',{ payload: req.body, errors: validator.errors });
+      res.status(400).json({message: 'You must indicate the correct data'});
       return;
     }
 
-    let result = await conversionManager.save(payload);
+    let result = await conversionManager.save({base, to, conversionFactor});
     res.json(result);
   }
 };
 
-module.exports = ({app, logger}) => {
+module.exports = ({app, logger, schemaValidator}) => {
   _logger = logger;
+  _schemaValidator = schemaValidator;
+
   app.post(`/api/conversions`, handler.convert);
   app.put(`/api/conversions`, handler.save);
 };
